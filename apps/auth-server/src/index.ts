92c341b402aa4c08f6342f339832a6bea128863e
Nadzeya Amialchuk <timoshenkova.nadya@gmail.com>
2025-05-05 12:01:13 +0300

    fix: fix imports to commonjs, fix in tsconfig

--- apps/auth-server/src/index.ts
+++ apps/auth-server/src/index.ts
@@ -1,6 +1,12 @@
-import app from './app';
-import prisma from './prisma/prisma.config';
 
+const express = require("express");
+const dotenv = require("dotenv");
+const prisma = require('./config/prisma.config')
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,7 +16,7 @@ prisma.$connect()
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
-  .catch((err) => {
+  .catch((err: Error) => {
     console.error('Database connection failed:', err);
     process.exit(1);
   });
